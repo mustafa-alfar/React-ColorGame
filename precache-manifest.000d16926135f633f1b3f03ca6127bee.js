7798c6c4e71720694bc951ba0a7681d550744ac1
mustafa alfar <mustafa.alfar95@gmail.com>
2018-11-29 23:12:24 +0200

    Updates

---
+++ precache-manifest.000d16926135f633f1b3f03ca6127bee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dfc916a1bc37db28215",
+    "url": "/React-ColorGame/static/js/runtime~main.7dfc916a.js"
+  },
+  {
+    "revision": "d0bc45d0f8e527fc6f32",
+    "url": "/React-ColorGame/static/js/main.d0bc45d0.chunk.js"
+  },
+  {
+    "revision": "dbe2cda11cb7b27817d4",
+    "url": "/React-ColorGame/static/js/1.dbe2cda1.chunk.js"
+  },
+  {
+    "revision": "d0bc45d0f8e527fc6f32",
+    "url": "/React-ColorGame/static/css/main.dba51be5.chunk.css"
+  },
+  {
+    "revision": "36ef8d0d19361849ff3c3e3350d5db3b",
+    "url": "/React-ColorGame/index.html"
+  }
+];
